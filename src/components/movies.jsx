d7727791d577fbf89ae83444aee46eef42f63878
Abhishek Singh <33527786+abhisheksingh98@users.noreply.github.com>
2019-03-15 23:30:04 +0530

    Update movies.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies } from "../services/fakeMovieService"
 
 class Movies extends Component {
   state = {
